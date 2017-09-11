d26944b8048a7d98e12b8aecd09cc994173b5006
Brent Bard Baker <brentbaker@hardy.cs.arizona.edu>
2017-09-11 13:00:33 -0700

    first red leader update

--- Hello.java
+++ Hello.java
@@ -11,6 +11,7 @@ public class Hello {
     public static void callSign() {
         
         // Red Leader should add a print statement that says "Lock S-foils in attack position." 
+	system.out.print(“Lock S-foils in attack position.”);
         // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
 
     }
